1d8734c037b536e2e834aeedaa44366431e30cc3
Sagar Duhan <sagar.duhan1@gmail.com>
2018-06-18 01:29:39 +0530

    Initial Commit

---
+++ rainyshinycloudy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rainyshinycloudy
+//
+//  Created by Sagar Duhan on 18/06/18.
+//  Copyright © 2018 Sagar Duhan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
